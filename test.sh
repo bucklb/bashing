e4d026cb8db04245716e0ca11ed301d5a78e035f
bob buckley <bob.buckley@dwp.gsi.gov.uk>
2018-05-14 13:18:41 +0100

    Lose bug. Add other way of doing this

--- test.sh
+++ test.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
+# show a sign of life
 echo Testing
 
 b=6
 e=8
-n=7
+n=9
 
-if [ [ $n -gt $b ] && [ $n -lt $e ] ]
+if [ $n -gt $b ] && [ $n -lt $e ]
 then
-	echo here
+	echo in Interval
+else
+	if [ $n -gt $b ]
+	then
+		echo AFTER
+	else
+		echo Before
+	fi
 fi
 
 
 
+
+
+
+
+# After the job should have started
 if [ $n -gt $b ] 
 then
+	# Before the job should have finsihed
 	if [ $n -lt $e ]
 	then
+		# Shouldn't be alerting at all
 		echo interval
 	else
+		# Should alert if job hasn't run today
 		echo After
 	fi
 else
+	# Should alert if job wasn't run yesterday
 	echo Before
 fi
 
